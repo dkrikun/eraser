20f02e74562d09fc7908eb29237071293f4c5aeb
Daniel Krikun <krikun.daniel@gmail.com>
2011-08-14 21:07:38 +0300

    Added previously forgotten lock.h

---
+++ src/eraser/lock.h
@@ -0,0 +1,52 @@
+#ifndef LOCK_H
+#define LOCK_H
+
+#include <jvmti.h>
+#include <iostream>
+#include <boost/operators.hpp>
+#include <boost/concept_check.hpp>
+
+#include "eraser/agent.h"
+
+namespace eraser
+{
+
+struct lock : boost::operators<lock>
+{
+	jobject lock_id_;
+
+	explicit lock( jobject lock_id )
+		: lock_id_( lock_id )
+	{}
+
+	// using default copy-ctor & assignment;
+	bool operator==( const lock& rhs ) const
+	{
+		bool res = agent::instance()->jni()->IsSameObject( lock_id_, rhs.lock_id_ );
+		ERASER_LOG( "lock compare res: " << std::boolalpha << res );
+		return res;
+	}
+};
+BOOST_CONCEPT_ASSERT((boost::Assignable<lock>));
+BOOST_CONCEPT_ASSERT((boost::EqualityComparable<lock>));
+
+inline std::size_t hash_value( const lock& l )
+{
+	jvmtiError err;
+	jint hash_code;
+	err = agent::instance()->jvmti()->GetObjectHashCode( l.lock_id_, &hash_code );
+	check_jvmti_error(agent::instance()->jvmti(), err, "get object hash code");
+	std::size_t res = static_cast<std::size_t>(hash_code);
+	ERASER_LOG( "lock hash=" << res );
+	return res;
+}
+
+inline std::ostream& operator<<( std::ostream& os, const lock& l )
+{
+	os << l.lock_id_;
+	return os;
+}
+
+};
+
+#endif
