2b71b2e40549de0a1d9e54b8c49e15973dff262d
Daniel Krikun <krikun.daniel@gmail.com>
2011-08-23 19:45:30 +0300

    Added previously forgotten threads_manage.cpp

---
+++ src/eraser/threads_manage.cpp
@@ -0,0 +1,29 @@
+
+
+#include <jvmti.h>
+#include <boost/assert.hpp>
+#include "eraser/threads_manage.h"
+#include "eraser/tag.h"
+#include "eraser/agent.h"
+
+namespace eraser
+{
+
+thread_t* get_thread( jthread thread_id, std::string debug )
+{
+	thread_t* thread = get_tag<thread_t>( thread_id );
+	if( thread != 0 )
+		BOOST_ASSERT_MSG( agent::instance()->jni()->IsSameObject( thread_id, thread->thread_id_ ) == JNI_TRUE
+				, debug.c_str() );
+	return thread;
+}
+
+thread_t* get_thread( jthread thread_id )
+{
+	thread_t* thread = get_tag<thread_t>( thread_id );
+	if( thread != 0 )
+		BOOST_ASSERT( agent::instance()->jni()->IsSameObject( thread_id, thread->thread_id_ ) == JNI_TRUE );
+	return thread;
+}
+
+}
