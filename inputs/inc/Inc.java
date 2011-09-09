d826f9d3bccbfe1082d792491958f2e716d7c5f2
Daniel Krikun <krikun.daniel@gmail.com>
2011-09-09 16:42:44 +0300

    Updated input samples

--- inputs/inc/Inc.java
+++ inputs/inc/Inc.java
@@ -17,6 +17,7 @@ public class Inc
                         case 1: two_synched_workers(); break;
                         case 2: two_method_synched_workers(); break;
                         case 3: two_synched_workers_and_main_thread_unsynched_but_initialization_issue_false_negative(); break;
+                        case 4: three_different_workers(); break;
                         default: System.err.println( "no such scenario" ); return;
                 }
 
@@ -36,7 +37,7 @@ public class Inc
         }
         public static void two_synched_workers()
         {
-                int num_iter = 5 ;
+                int num_iter = 10 ;
                 Cell cell = new Cell();
 
                 Worker w1 = new SynchWorker( 1, cell, num_iter ); 
@@ -48,7 +49,7 @@ public class Inc
 
         public static void two_method_synched_workers()
         {
-                int num_iter = 5 ;
+                int num_iter = 10 ;
                 Cell cell = new Cell();
 
                 Worker w1 = new MethodSynchWorker( 1, cell, num_iter ); 
@@ -60,7 +61,7 @@ public class Inc
 
         public static void two_synched_workers_and_main_thread_unsynched_but_initialization_issue_false_negative()
         {
-                int num_iter = 5 ;
+                int num_iter = 10 ;
                 Cell cell = new Cell();
 
                 Worker w1 = new SynchWorker( 1, cell, num_iter ); 
@@ -70,5 +71,20 @@ public class Inc
 
                 System.out.println("Result: " + cell.data + " " + cell.id);
                 System.out.println( "Expected no alarms" );
+        }
+
+        public static void three_different_workers()
+        {
+                int num_iter = 10 ;
+                Cell cell = new Cell();
+
+                Worker w1 = new SynchWorker( 1, cell, num_iter ); 
+                Worker w2 = new Worker( 2, cell, num_iter ); 
+                Worker w3 = new MethodSynchWorker( 3, cell, num_iter ); 
+                w1.start();
+                w2.start();
+                w3.start();
+
+                System.out.println( "Expected alarms here" );
         }
 } 
